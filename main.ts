ff171e65d718f5fb865fd207c2ec92fe3131174d
KimahriRonzo <98688795+KimahriRonzo@users.noreply.github.com>
2022-01-30 14:41:50 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,47 @@ input.onButtonPressed(Button.A, function () {
     MarioUnten = 0
     led.unplot(1, 3)
     led.plot(1, 2)
-    basic.pause(200)
+    basic.pause(100)
     led.unplot(1, 2)
     led.plot(1, 1)
-    basic.pause(200)
+    basic.pause(100)
     led.unplot(1, 1)
     led.plot(1, 2)
-    basic.pause(200)
+    basic.pause(100)
     led.unplot(1, 2)
     led.plot(1, 3)
     MarioUnten = 1
     AnzahlSprünge = AnzahlSprünge + 1
     if (AnzahlSprünge == 3) {
+        Win = 1
         basic.showString("WIN!")
     }
 })
 function starteNeu () {
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(100)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -27,51 +52,51 @@ function starteNeu () {
         `)
     AnzahlSprünge = 0
     MarioUnten = 1
+    Kistenposition = 4
+    Win = 0
 }
+function verschiebeKiste () {
+    led.unplot(Kistenposition, 3)
+    if (Kistenposition == 0) {
+        Kistenposition = 4
+    } else {
+        Kistenposition = Kistenposition - 1
+        if (Win == 1) {
+            starteNeu()
+        }
+        if (MarioUnten == 1 && Kistenposition == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # . .
+                # . # . .
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . . . # .
+                # # # # #
+                `)
+            basic.showLeds(`
+                . # . . #
+                . . . . .
+                . . . . .
+                . . . . #
+                # # # # #
+                `)
+            starteNeu()
+        }
+        led.plot(Kistenposition, 3)
+        basic.pause(200)
+    }
+}
+let Kistenposition = 0
+let Win = 0
 let AnzahlSprünge = 0
 let MarioUnten = 0
 starteNeu()
 basic.forever(function () {
-    led.unplot(4, 3)
-    led.plot(3, 3)
-    basic.pause(200)
-    led.unplot(3, 3)
-    led.plot(2, 3)
-    basic.pause(200)
-    led.unplot(2, 3)
-    if (MarioUnten == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # . .
-            # . # . .
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . # .
-            # # # # #
-            `)
-        basic.showLeds(`
-            . # . . #
-            . . . . .
-            . . . . .
-            . . . . #
-            # # # # #
-            `)
-        starteNeu()
-    } else {
-        led.plot(1, 3)
-        basic.pause(200)
-        led.unplot(1, 3)
-        basic.pause(200)
-        led.plot(0, 3)
-        basic.pause(200)
-        led.unplot(0, 3)
-        basic.pause(200)
-        led.plot(4, 3)
-        basic.pause(200)
-    }
+    verschiebeKiste()
 })
