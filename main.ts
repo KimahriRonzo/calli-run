6e615544018e0172581d5091e674a9082c7f7f92
KimahriRonzo <98688795+KimahriRonzo@users.noreply.github.com>
2022-01-30 11:39:08 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(1, 3)
+    led.plot(1, 2)
+    basic.pause(200)
+    led.unplot(1, 2)
+    led.plot(1, 1)
+    basic.pause(200)
+    led.unplot(1, 1)
+    led.plot(1, 2)
+    basic.pause(200)
+    led.unplot(1, 2)
+    led.plot(1, 3)
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . # . . .
+    # # # # #
+    `)
+basic.forever(function () {
+	
+})
 basic.forever(function () {
 	
 })
