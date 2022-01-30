5fae18ac9572ec1ee59749ab9017b2c8339e0157
KimahriRonzo <98688795+KimahriRonzo@users.noreply.github.com>
2022-01-30 13:47:42 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,54 +11,31 @@ input.onButtonPressed(Button.A, function () {
     led.unplot(1, 2)
     led.plot(1, 3)
 })
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . # . . .
-    # # # # #
-    `)
 basic.forever(function () {
-    let Zaehler = 0
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . # . . #
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . # . # .
-        # # # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . # . .
-        # # # # #
-        `)
+    led.plot(4, 3)
+    basic.pause(500)
+    led.unplot(4, 3)
+    led.plot(3, 3)
+    basic.pause(500)
+    led.unplot(3, 3)
+    led.plot(2, 3)
+    basic.pause(200)
+    led.unplot(2, 3)
+    led.plot(1, 3)
+    basic.pause(500)
+    led.unplot(1, 3)
+    basic.pause(500)
+    led.plot(0, 3)
+    basic.pause(500)
+    led.unplot(0, 3)
+    basic.pause(500)
+})
+basic.forever(function () {
     basic.showLeds(`
         . . . . .
         . . . . .
         . . . . .
         . # . . .
-        # # # # #
-        `)
-    if (led.point(Zaehler, Zaehler)) {
-        basic.showString("Lose!")
-    }
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . .
         # # # # #
         `)
 })
